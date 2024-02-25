2a0fd0d12626b28d077526f8550c21c39c8602d2
Tawfiq Khalilieh <taw.coding@gmail.com>
2024-02-25 21:33:58 +0200

    hii im just testing and improving my copy-pasting superpowergit add .!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -15,6 +19,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // rest of the code
+    ({ addBase, theme }: any) => {
+      let allColors = flattenColorPalette(theme("colors"));
+      let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+      );
+
+      addBase({
+        ":root": newVars,
+      });
+    },
+  ],
 };
+
 export default config;
